85c058b8f1638d059741ad122d28ab312564e637
Palash Das <dpalash.new@gmail.com>
2020-04-03 01:13:14 +0530

    Insert hack to pretty print elements with empty method body

--- interface-wrapper/src/main/java/ppanda/sharpie/tools/interfacewrapper/processors/readers/AbstractSourceReader.java
+++ interface-wrapper/src/main/java/ppanda/sharpie/tools/interfacewrapper/processors/readers/AbstractSourceReader.java
@@ -4,12 +4,16 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.processing.PrintingProcessor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.io.Writer;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.util.Elements;
 import ppanda.sharpie.tools.interfacewrapper.processors.ProcessingComponent;
 
 public abstract class AbstractSourceReader extends ProcessingComponent {
@@ -26,7 +30,7 @@ public abstract class AbstractSourceReader extends ProcessingComponent {
         }
     }
 
-    //TODO: Does not read method bodies
+    //TODO: Does not read original method bodies, replaced by PrinterWithMarkerForMethodBodies.EMPTY_METHOD_BODY
     protected ClassOrInterfaceDeclaration findTypeByPrintingElement(Element element) {
         String s = getSourceByElementPrinting(element);
         CompilationUnit compilationUnit = StaticJavaParser.parse(s);
@@ -35,8 +39,13 @@ public abstract class AbstractSourceReader extends ProcessingComponent {
 
     protected String getSourceByElementPrinting(Element element) {
         StringWriter writer = new StringWriter();
-        processingEnv().getElementUtils().printElements(writer, element);
-        return writer.getBuffer().toString();
+        //TODO: Worst hack in entire code base to make source parsable, this adds empty method bodies without thinking
+        new PrinterWithMarkerForMethodBodies(writer, processingEnv().getElementUtils()).visit(element).flush();
+        String sourceWithoutMethodBodies = writer.getBuffer().toString();
+
+        return sourceWithoutMethodBodies.replaceAll(
+            ";\\n" + PrinterWithMarkerForMethodBodies.STUPID_UNIQUE_MARKER,
+            PrinterWithMarkerForMethodBodies.EMPTY_METHOD_BODY);
     }
 
     //TODO: Does not read in qualified names for type declarations
@@ -62,5 +71,30 @@ public abstract class AbstractSourceReader extends ProcessingComponent {
             .findAny()
             .map(t -> (ClassOrInterfaceDeclaration) t)
             .orElseThrow(() -> new RuntimeException("No class found for " + element + " inside " + compilationUnit));
+    }
+
+    private static class PrinterWithMarkerForMethodBodies extends PrintingProcessor.PrintingElementVisitor {
+
+        public static final String EMPTY_METHOD_BODY = " {  } ";
+        public static final String STUPID_UNIQUE_MARKER = "TAA_DOOM_FISSSS_b1beb89bf12df689";
+        private final Writer writer;
+
+        public PrinterWithMarkerForMethodBodies(Writer writer, Elements elements) {
+            super(writer, elements);
+            this.writer = writer;
+        }
+
+        @Override
+        public PrintingProcessor.PrintingElementVisitor visitExecutable(ExecutableElement element, Boolean aBoolean) {
+            PrintingProcessor.PrintingElementVisitor visitor = super.visitExecutable(element, aBoolean);
+            try {
+                writer.write(STUPID_UNIQUE_MARKER);
+                writer.write("\n");
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return visitor;
+        }
     }
 }
